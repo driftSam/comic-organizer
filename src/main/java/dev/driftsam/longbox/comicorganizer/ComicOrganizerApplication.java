a1bf7dc39e9654f2b564e6d790c270063306ae43
pete <pjjuliana216@gmail.com>
2022-09-30 23:52:30 -0400

    init commit

---
+++ src/main/java/dev/driftsam/longbox/comicorganizer/ComicOrganizerApplication.java
@@ -0,0 +1,13 @@
+package dev.driftsam.longbox.comicorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/driftsam/longbox/comicorganizer/ComicOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.driftsam.longbox.comicorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
